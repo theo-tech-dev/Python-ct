46b6e31e1f6e1d93f63a8c43b46df25e2026520a
theo-tech-dev <147119615+theo-tech-dev@users.noreply.github.com>
2023-11-14 20:52:40 -0500

    Create Jupyter_Assignment.py

---
+++ Jupyter_Assignment.py
@@ -0,0 +1,60 @@
+1) Int & Float assignments
+     a) # Assigning int
+
+        my_int = 10
+        print(my_int)
+
+     b) # Assigning float
+        
+        my_float = 3.14
+        print(my_float)
+
+     c) Performing Calculations on ints and floats
+         # Addition
+            add = my_int + my_float
+
+         # Subtraction
+            subtraction = my_int - my_float
+
+         # Multiplication
+            multiplication = my_int * my_float
+
+         # Division
+            division = my_int / my_float
+
+         # Floor Division
+            floor division = my_int // my_float
+         
+         # Modulo
+            modulo = my_int % my_float
+         
+         # Exponential
+            exponential = my_int ** my_float
+
+2) String Input-Output
+     a) # String Assignment
+
+        string = "Hello World"
+
+     b) print(string)
+
+     c) # String Concatenation
+        string = "Hello" + " " + "World"
+
+     d) # Type Conversion        
+        string = "Hello" + " " + str(10)
+     e) input(string = "Hello" + " " + str(10)
+              
+     f) # format()
+
+     g) #  Old Way (python 2)
+        result_string ="Hello World" % ("Hello", "World")   
+3) In-Class Exercise 
+name = input ("What is your name? ")
+color = input ("What color is your car? ")
+year = input ("What year is your car? ")
+model = input ("What model is your car? ")
+results = f"(name) , your (year) (color) (model) is a nice car!"
+
+print(results)
+
